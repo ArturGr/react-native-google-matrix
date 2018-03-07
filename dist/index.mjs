37f365698c4d3f2b92d78ba9150afda2e242dbdf
ArturGr <graefenstein@petesso.de>
2018-03-08 00:34:18 +0200

    Update index.mjs
    fix path

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,6 +1,6 @@
 /*global fetch, */
 'use strict';
-var qs = require('../../query-string);
+var qs = require('../../query-string');
 
 
 const DISTANCE_API_URL = 'https://maps.googleapis.com/maps/' +
